b2f92ea61d1cb7ef58dcf9feaa9d943754b85f91
arigato <teeruch.k@gmail.com>
2019-12-20 21:40:09 +0700

    os modules

---
+++ reference/os_demo.js
@@ -0,0 +1,20 @@
+const os = require('os');
+
+// Platform os
+// console.log(os.platform());
+
+// CPU Arch
+// console.log(os.arch());
+
+// CPU Core Info
+// console.log(os.cpus());
+
+// Free memory
+// console.log(os.freemem());
+// console.log(os.totalmem());
+
+// Home dir
+// console.log(os.homedir());
+
+// Uptime
+console.log(os.uptime());
