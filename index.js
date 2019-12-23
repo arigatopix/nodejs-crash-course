874fc30927d135379c2672e3840c854642bf7551
arigatopix <teeruch.k@gmail.com>
2019-12-23 09:27:44 +0700

    Event Logger

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const logger = new Logger();
 
 logger.on('message', data => console.log('Called Listener: ', data));
 
+// เรียก function ใน class
 logger.log('Hello World');
 logger.log('Hi');
 logger.log('Hello');
